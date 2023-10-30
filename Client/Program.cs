f982941c0119afe900013d43f7dd2ee5fea5ebfb
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-10-30 03:46:45 -0400

    Login del primer video terminado

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,16 @@
+global using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProjectLogin.Client;
+using PruebaAutenticacion.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
